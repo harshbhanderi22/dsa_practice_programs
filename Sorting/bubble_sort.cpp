257d7be8c176136306144245fbd631924e59b38d
Harsh Bhanderi <harshbhanderi188@gmail.com>
2022-09-20 16:05:27 +0530

    Bubble sort updated

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -3,12 +3,22 @@ using namespace std;
 
 void bubblesort(int a[],int n){
     for(int i=0;i<n;i++){
+
+        //It will be optimised as it will give indication if all things are sorted
+        //For sorted array, no swapping will happen 
+        bool swapped = false;
         for(int j = 0;j<n-i-1;j++){
             if(a[j]>a[j+1]){
                 int temp = a[j];
                 a[j]=a[j+1];
                 a[j+1]=temp;
+                swapped = true;
             }
+        }
+
+        //If swapping will not happend which means array is already sorted so no need to check other levels.
+        if(swapped==false){
+            break;
         }
     }
 
